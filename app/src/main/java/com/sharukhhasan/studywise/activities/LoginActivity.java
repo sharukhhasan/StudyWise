0efee8e560a8c14754639e4cceaebd8802388da8
Sharukh Hasan <sharukh-hasan@uiowa.edu>
2016-02-13 04:45:36 -0600

    backend changes

--- app/src/main/java/com/sharukhhasan/studywise/activities/LoginActivity.java
+++ app/src/main/java/com/sharukhhasan/studywise/activities/LoginActivity.java
@@ -1,42 +1,38 @@
 package com.sharukhhasan.studywise.activities;
 
-import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.facebook.AccessToken;
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.mobileconnectors.cognito.CognitoSyncManager;
+import com.amazonaws.mobileconnectors.cognito.Dataset;
+import com.amazonaws.mobileconnectors.cognito.DefaultSyncCallback;
+import com.amazonaws.regions.Regions;
+
 import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.GraphRequest;
-import com.facebook.GraphResponse;
-import com.facebook.HttpMethod;
-import com.facebook.login.LoginManager;
-import com.facebook.login.LoginResult;
-import com.firebase.client.AuthData;
-import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
+import com.facebook.FacebookSdk;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.AccessToken;
+
 import com.sharukhhasan.studywise.R;
 
-import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
-import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 public class LoginActivity extends AppCompatActivity
 {
 
     private static final String TAG = "LoginActivity";
+    private static final String FIREBASE_URL = "https://studywise.firebaseio.com";
     private static final int REQUEST_SIGNUP = 0;
 
-    private Firebase ref;
-    private Button loginButton;
+    private LoginButton loginButton;
     private CallbackManager callbackManager;
 
     @InjectView(R.id.input_username)
@@ -47,122 +43,46 @@ public class LoginActivity extends AppCompatActivity
     @InjectView(R.id.link_signup)
     TextView _signupLink;
 
-    private com.facebook.GraphRequest.Callback friendsCallback = new GraphRequest.Callback()
-    {
-        @Override
-        public void onCompleted (GraphResponse response)
-        {
-            Log.v("FACEBOOK", response.getRawResponse());
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        FacebookSdk.sdkInitialize(getApplicationContext());
         callbackManager = CallbackManager.Factory.create();
 
-        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("user_friends", "user_photos"));
-        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult)
-            {
-                onFacebookAccessTokenChange(loginResult.getAccessToken());
-            }
+        loginButton = (LoginButton)findViewById(R.id.facebook_login_button);
 
-            @Override
-            public void onCancel ()
-            {
-                //App code
-            }
+        // Initialize the Amazon Cognito credentials provider
+        CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
+                getApplicationContext(),
+                "us-east-1:0a3b862e-4534-4a7a-b37a-3bd0a46bc67d", // Identity Pool ID
+                Regions.US_EAST_1 // Region
+        );
 
+        // Initialize the Cognito Sync client
+        CognitoSyncManager syncClient = new CognitoSyncManager(
+                getApplicationContext(),
+                Regions.US_EAST_1, // Region
+                credentialsProvider);
+
+        CognitoSyncClient syncClient = new DefaultCognitoSyncClient(myActivity.getContext(),
+                COGNITO_POOL_ID, cognitoProvider);
+
+        // Create a record in a dataset and synchronize with the server
+        Dataset dataset = syncClient.openOrCreateDataset("myDataset");
+        dataset.put("myKey", "myValue");
+        dataset.synchronize(new DefaultSyncCallback()
+        {
             @Override
-            public void onError (FacebookException exception)
+            public void onSuccess(Dataset dataset, List newRecords)
             {
-                //App code
+                //Your handler code here
             }
         });
 
-        ButterKnife.inject(this);
 
-        View myView = this.findViewById(android.R.id.content);
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
-        {
-            myView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
-        }
-
-        _loginButton.setOnClickListener(new View.OnClickListener()
-        {
-
-            @Override
-            public void onClick(View v)
-            {
-                String username = usernameInput.getText().toString();
-                String password = passwordInput.getText().toString();
-                //loginUser(username.toLowerCase(Locale.getDefault()), password);
-            }
-        });
-
-        _signupLink.setOnClickListener(new View.OnClickListener()
-        {
-
-            @Override
-            public void onClick(View v)
-            {
-                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
-                startActivityForResult(intent, REQUEST_SIGNUP);
-            }
-        });
-
-        Firebase.setAndroidContext(this);
-
-        ref = new Firebase("https://studywise.firebaseIO.com");
-
-    }
-
-    @Override
-    protected void onActivityResult (int requestCode, int resultCode, Intent data)
-    {
-        super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
-    }
-
-    private void onFacebookAccessTokenChange (AccessToken token)
-    {
-        if (token != null) {
-            ref.authWithOAuthToken("facebook", token.getToken(), new Firebase.AuthResultHandler()
-            {
-
-                @Override
-                public void onAuthenticated(AuthData authData)
-                {
-                    //The Facebook user is now authenticated with your Firebase app
-                    getFacebookFriends();
-                }
-
-                @Override
-                public void onAuthenticationError(FirebaseError firebaseError)
-                {
-                    //there was an error
-                }
-            });
-        }
-        else
-        {
-            /* Logged out of Facebook so do a logout from the Firebase app */
-            ref.unauth();
-        }
-    }
-
-    private void getFacebookFriends()
-    {
-        Bundle parameters = new Bundle();
-        parameters.putString("redirect", "false");
-        new GraphRequest(AccessToken.getCurrentAccessToken(), "/me/picture", parameters,
-                HttpMethod.GET, friendsCallback).executeAsync();
-    }
 
 
 }
