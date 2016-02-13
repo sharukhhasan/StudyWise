b0b0d7a779d5801cfbbbadd740412be5bc46a9d4
Sharukh Hasan <sharukh-hasan@uiowa.edu>
2016-02-13 17:38:28 -0600

    splash activity and login methods

--- app/src/main/java/com/sharukhhasan/studywise/AppController.java
+++ app/src/main/java/com/sharukhhasan/studywise/AppController.java
@@ -1,19 +1,29 @@
 package com.sharukhhasan.studywise;
 
 import android.app.Application;
+import android.support.multidex.MultiDexApplication;
+import android.util.Log;
 
-import com.facebook.FacebookSdk;
-import com.firebase.client.Firebase;
+import com.sharukhhasan.amazonaws.mobile.AWSMobileClient;
 
 /**
  * Created by Sharukh on 2/12/16.
  */
-public class AppController extends Application
+public class AppController extends MultiDexApplication
 {
+    private final static String LOG_TAG = Application.class.getSimpleName();
+
     @Override
     public void onCreate() {
+        Log.d(LOG_TAG, "Application.onCreate - Initializing application...");
         super.onCreate();
-        FacebookSdk.sdkInitialize(this);
-        Firebase.setAndroidContext(this);
+        initializeApplication();
+        Log.d(LOG_TAG, "Application.onCreate - Application initialized OK");
+    }
+
+    private void initializeApplication() {
+        AWSMobileClient.initializeMobileClientIfNecessary(getApplicationContext());
+
+        // ...Put any application-specific initialization logic here...
     }
 }
--- app/src/main/java/com/sharukhhasan/studywise/activities/SplashActivity.java
+++ app/src/main/java/com/sharukhhasan/studywise/activities/SplashActivity.java
@@ -1,22 +1,166 @@
 package com.sharukhhasan.studywise.activities;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-
-import com.firebase.client.Firebase;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.Toast;
+import com.sharukhhasan.amazonaws.mobile.AWSMobileClient;
+import com.sharukhhasan.amazonaws.mobile.user.signIn.SignInManager;
+import com.sharukhhasan.amazonaws.mobile.user.signIn.SignInProvider;
+import com.sharukhhasan.amazonaws.mobile.user.IdentityManager;
+import com.sharukhhasan.amazonaws.mobile.user.IdentityProvider;
 import com.sharukhhasan.studywise.R;
 
-public class SplashActivity extends AppCompatActivity
+import java.util.concurrent.CountDownLatch;
+
+public class SplashActivity extends Activity
 {
+    private final static String LOG_TAG = SplashActivity.class.getSimpleName();
+    private final CountDownLatch timeoutLatch = new CountDownLatch(1);
+    private SignInManager signInManager;
+
+    /**
+     * SignInResultsHandler handles the results from sign-in for a previously signed in user.
+     */
+    private class SignInResultsHandler implements IdentityManager.SignInResultsHandler {
+        /**
+         * Receives the successful sign-in result for an alraedy signed in user and starts the main
+         * activity.
+         * @param provider the identity provider used for sign-in.
+         */
+        @Override
+        public void onSuccess(final IdentityProvider provider) {
+            Log.d(LOG_TAG, String.format("User sign-in with previous %s provider succeeded",
+                    provider.getDisplayName()));
+
+            // The sign-in manager is no longer needed once signed in.
+            SignInManager.dispose();
+
+            Toast.makeText(SplashActivity.this, String.format("Sign-in with %s succeeded.",
+                    provider.getDisplayName()), Toast.LENGTH_LONG).show();
+
+            AWSMobileClient.defaultMobileClient()
+                    .getIdentityManager()
+                    .loadUserInfoAndImage(provider, new Runnable() {
+                        @Override
+                        public void run() {
+                            goMain();
+                        }
+                    });
+        }
+
+        /**
+         * For the case where the user previously was signed in, and an attempt is made to sign the
+         * user back in again, there is not an option for the user to cancel, so this is overriden
+         * as a stub.
+         * @param provider the identity provider with which the user attempted sign-in.
+         */
+        @Override
+        public void onCancel(final IdentityProvider provider) {
+            Log.wtf(LOG_TAG, "Cancel can't happen when handling a previously sign-in user.");
+        }
+
+        /**
+         * Receives the sign-in result that an error occurred signing in with the previously signed
+         * in provider and re-directs the user to the sign-in activity to sign in again.
+         * @param provider the identity provider with which the user attempted sign-in.
+         * @param ex the exception that occurred.
+         */
+        @Override
+        public void onError(final IdentityProvider provider, Exception ex) {
+            Log.e(LOG_TAG,
+                    String.format("Cognito credentials refresh with %s provider failed. Error: %s",
+                            provider.getDisplayName(), ex.getMessage()), ex);
+
+            Toast.makeText(SplashActivity.this, String.format("Sign-in with %s failed.",
+                    provider.getDisplayName()), Toast.LENGTH_LONG).show();
+            goSignIn();
+
+        }
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d(LOG_TAG, "onCreate");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        Firebase.setAndroidContext(this);
+        final Thread thread = new Thread(new Runnable() {
+            public void run() {
+                signInManager = SignInManager.getInstance(SplashActivity.this);
 
+                final SignInProvider provider = signInManager.getPreviouslySignedInProvider();
 
+                // if the user was already previously in to a provider.
+                if (provider != null) {
+                    // asynchronously handle refreshing credentials and call our handler.
+                    signInManager.refreshCredentialsWithProvider(SplashActivity.this,
+                            provider, new SignInResultsHandler());
+                } else {
+                    // Asyncronously go to the sign-in page (after the splash delay has expired).
+                    goSignIn();
+                }
+
+                // Wait for the splash timeout.
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) { }
+
+                // Expire the splash page delay.
+                timeoutLatch.countDown();
+            }
+        });
+        thread.start();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // Touch event bypasses waiting for the splash timeout to expire.
+        timeoutLatch.countDown();
+        return true;
+    }
+
+    /**
+     * Starts an activity after the splash timeout.
+     * @param intent the intent to start the activity.
+     */
+    private void goAfterSplashTimeout(final Intent intent) {
+        final Thread thread = new Thread(new Runnable() {
+            public void run() {
+                // wait for the splash timeout expiry or for the user to tap.
+                try {
+                    timeoutLatch.await();
+                } catch (InterruptedException e) {
+                }
+
+                SplashActivity.this.runOnUiThread(new Runnable() {
+                    public void run() {
+                        startActivity(intent);
+                        // finish should always be called on the main thread.
+                        finish();
+                    }
+                });
+            }
+        });
+        thread.start();
+    }
+
+    /**
+     * Go to the main activity after the splash timeout has expired.
+     */
+    protected void goMain() {
+        Log.d(LOG_TAG, "Launching Main Activity...");
+        goAfterSplashTimeout(new Intent(this, HomeActivity.class));
+    }
+
+    /**
+     * Go to the sign in activity after the splash timeout has expired.
+     */
+    protected void goSignIn() {
+        Log.d(LOG_TAG, "Launching Sign-in Activity...");
+        goAfterSplashTimeout(new Intent(this, LoginActivity.class));
     }
 }
